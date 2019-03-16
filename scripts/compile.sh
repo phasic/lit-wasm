198b4fd84af208b52eff824a0df51108fbcb3ca0
yoran <yoran@cobben.com>
2019-03-16 12:45:21 +0100

    updated compile script

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -1,15 +1,28 @@
 #!/bin/bash
-cd ./src
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NC='\033[0m'
 
-compile() { 
-  FILENAME="$(echo "$1" | sed "s/.*\///")"
-  echo $FILENAME
-  BASENAME="$(echo ${FILENAME%.*})"
-  echo $BASENAME
-  DIR=$(dirname "$1")
-  echo $DIR
+{ 
+  cd ./src
 
-  emcc $1 -s WASM=1 -O3 -o $DIR/$BASENAME.js
- }
-export -f compile
-find . -name "*.c" -exec bash -c 'compile "$@"' bash {} +
+  compile() { 
+    FILENAME="$(echo "$1" | sed "s/.*\///")"
+    BASENAME="$(echo ${FILENAME%.*})"
+    DIR=$(dirname "$1")
+
+    CYAN='\033[0;36m'
+    printf "${CYAN}Compiling ${1}${NC}\n"
+    
+    emcc $1 -s WASM=1 -O3 -o $DIR/$BASENAME.js
+  }
+  
+  export -f compile
+  find . -name "*.c" -exec bash -c 'compile "$@"' bash {} +
+
+    printf "${GREEN}Succesfully compiled resources!${NC}\n"
+
+
+} || {
+    printf "${RED}Something went wrong${NC}\n"
+}
